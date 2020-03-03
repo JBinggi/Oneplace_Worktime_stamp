80d49e30f05d164484e11c0e327567212b36bce7
JBinggi <juerg.binggeli@gmail.com>
2020-03-03 18:18:44 +0100

    change packed to JBinggi

--- src/Module.php
+++ src/Module.php
@@ -13,7 +13,7 @@
  * @since 1.0.0
  */
 
-namespace OnePlace\Worktime\Stamp;
+namespace JBinggi\Worktime\Stamp;
 
 use Application\Controller\CoreEntityController;
 use Laminas\Mvc\MvcEvent;
@@ -22,8 +22,8 @@ use Laminas\Db\TableGateway\TableGateway;
 use Laminas\Db\Adapter\AdapterInterface;
 use Laminas\EventManager\EventInterface as Event;
 use Laminas\ModuleManager\ModuleManager;
-use OnePlace\Worktime\Stamp\Controller\StampController;
-use OnePlace\Worktime\Stamp\Model\StampTable;
+use JBinggi\Worktime\Stamp\Controller\StampController;
+use JBinggi\Worktime\Stamp\Model\StampTable;
 
 class Module {
     /**
@@ -99,7 +99,7 @@ class Module {
                     $oDbAdapter = $container->get(AdapterInterface::class);
                     return new Controller\InstallController(
                         $oDbAdapter,
-                        $container->get(Model\AddressTable::class),
+                        $container->get(Model\StampTable::class),
                         $container
                     );
                 },
